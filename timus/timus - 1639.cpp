ee9b363853093ead2d8a11e1c6068b89cbe66272
Triplem5ds <mmmanany@yahoo.com>
2017-07-03 06:54:31 +0200

    Create timus - 1639.cpp

---
+++ timus/timus - 1639.cpp
@@ -0,0 +1,13 @@
+  #include <bits/stdc++.h>
+  using namespace std;
+
+  int main() {
+
+    int n, m;
+
+    cin >> n >> m;
+
+    if(m&1 && n & 1)return puts("[second]=:]");
+    else puts("[:=[first]");
+    return 0;
+  }     
