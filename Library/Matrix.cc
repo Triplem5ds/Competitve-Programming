ab3d015aba402bcc84ce1c17a82518c9b87c9ada
Triplem5ds <mmmanany@yahoo.com>
2019-02-09 09:36:44 +0200

    Update Matrix.cc

--- Library/Matrix.cc
+++ Library/Matrix.cc
@@ -1,20 +1,27 @@
-struct Matrix{
+struct Matrix {
 	ll mat[5][5];
-	Matrix(bool I){
+	Matrix(bool I) {
 		memset(mat, 0, sizeof mat);
-		if(I)
-			for(int j = 0; j < 5; j++)
+		if (I)
+			for (int j = 0; j < 5; j++)
 				mat[j][j] = 1;
 	}
-	int * operator [] (int i){
+	int * operator [](int i) {
 		return mat[i];
 	}
-	friend Matrix operator * (Matrix A, Matrix B){
+	friend Matrix operator *(Matrix A, Matrix B) {
 		Matrix ret;
-		for(int i = 0; i < 5; i++)
-			for(int j = 0; j < 5; j++)
-				for(int k = 0; k < 6; k++)
+		for (int i = 0; i < 5; i++)
+			for (int j = 0; j < 5; j++)
+				for (int k = 0; k < 6; k++)
 					ret[i][j] = (ret[i][j] + A[i][k] * B[k][j]) % MOD;
 		return ret;
+	}
+	Matrix expo(int n) {
+		if (!n)
+			return Matrix(1);
+		if (n & 1)
+			return *this * expo(n - 1);
+		return (*this * *this).expo(n >> 1);
 	}
 };
