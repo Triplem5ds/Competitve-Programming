7c509bc6aafaa9658caf6b4b07f4c95042fdf9ab
Triplem5ds <mmmanany@yahoo.com>
2019-02-13 00:36:51 +0200

    Update Segment Tree.cc

--- Library/Segment Tree.cc
+++ Library/Segment Tree.cc
@@ -1,81 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int  N = 1e5 + 5;
-class SegmentTree{
-private:
-  vector<int>tr, laz, a;
-  int n;
-  int pull(int x, int y){
-    return x + y;
-  }
-  void push(int node ,int s, int e){
-    if(laz[node]){
-      tr[node] = laz[node];
-      if(s != e){
-        laz[node<<1] = laz[node];
-        laz[node<<1|1] = laz[node];
-      }
-      laz[node] = 0;
-    }
-  }
-  void build(int node, int s, int e){
-    if(s == e){
-      tr[node] = a[s];
-      return;
-    }
-    int md = (s+e) >> 1;
-    build(node<<1, s, md);
-    build(node<<1|1, md + 1, e);
-    tree[node] = pull(tree[node<<1], tree[node<<1 | 1]);
-  }
-  int qry(int node, int s, int e, int l, int r){
-    //push(node,s,e);
-    if(r < s || e < l)return 0;
-    if(l <= s && e <= r)return tr[node];
-    int md = (s+e) >> 1;
-    return pull(qry(node<<1, s, md, l, r) , qry(node<<1|1, md + 1, e, l, r));
-  }
-  void update(int node, int s, int e, int l, int r, int v){
-    //push(node,s,e);
-    if(r < s || e < l)return;
-    if(l <= s && e <= r){
-      tr[node] = v;
-      //push(node,s,e);
-      return;
-    }
-    int md = (s+e) >> 1;
-    update(node<<1, s, md, l, r, v);
-    update(node<<1|1, md + 1, e, l, r, v);
-    tree[node] = pull(tree[node<<1], tree[node<<1 | 1]);
-  }
-public:
-  SegmentTree(vector<int> & x){
-    a = x;
-    n = x.size();
-    tr.resize(4 * n), laz.resize(4 * n);
-    build(1,0,n-1);
-  }
-  SegmentTree(int n, int * x){
-    a.resize(n);
-    for(int i = 0; i < n; i++)a[i] = x[i];
-    tr.resize(4 * n), laz.resize(4 * n);
-    build(1,0,n-1);
-  }
-  SegmentTree(int n, int v):n(n){
-    tr.assign(4 * n,v);
-    laz.resize(4 * n);
-  }
-  void updR(int l, int r, int v){
-    update(1,0,n-1,l,r,v);
-  }
-  void updI(int x, int v){
-    update(1,0,n-1,x,x,v);
-  }
-  int qry(int l, int r){
-    return qry(1,0,n-1,l,r);
-  }
-};
-int main(){
+const int N = 1e5 + 5;
+int t[N << 2];
+int lz[N << 2];
+void push(int node, int s, int e) {
+	if (s != e) {
+	}
+}
+void mrg(int a, int b) {
 
+}
+void build(int node, int s, int e) {
+	if (s == e) {
+		return;
+	}
+	int md = (s + e) >> 1;
+	build(node << 1, s, md);
+	build(node << 1 | 1, md + 1, e);
+	t[node] = mrg(t[node << 1], t[node << 1 | 1]);
+}
+void upd(int node, int s, int e, int l, int r, int v) {
+	push(node, s, e);
+	if (r < s || e < l) {
+		push(node, s, e);
+		return;
+	}
+	int md = (s + e) >> 1;
+	upd(node << 1, s, md, l, r, v);
+	upd(node << 1 | 1, md + 1, e, l, r, v);
+	t[node] = mrg(t[node << 1], t[node << 1 | 1]);
+}
+void updI(int node, int s, int e, int idx, int v) {
+	if (s == e) {
+		return;
+	}
+	int md = (s + e) >> 1;
+	if (idx <= md)
+		updI(node << 1, s, md, idx, v);
+	else
+		updI(node << 1 | 1, md + 1, e, idx, v);
+	t[node] = mrg(t[node << 1], t[node << 1 | 1]);
+}
+int qry(int node, int s, int e, int l, int r) {
+	push(node, s, e);
+	if (l <= s && e <= r) {
+		return t[node];
+	}
+	int md = (s + e) >> 1;
+	if (r <= md)
+		return qry(node << 1, s, md, l, r);
+	else if (l > md)
+		return qry(node << 1 | 1, md + 1, e, l, r);
+	auto x = qry(node << 1, s, md, l, r);
+	auto y = qry(node << 1 | 1, md + 1, e, l, r);
+	return mrg(x, y);
+}
+int main() {
 
 }
