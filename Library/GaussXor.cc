0b05485fa68afd45f93fce1161af60b3c320a67a
Triplem5ds <mmmanany@yahoo.com>
2022-09-01 22:10:15 +0200

    Update GaussXor.cc

--- Library/GaussXor.cc
+++ Library/GaussXor.cc
@@ -1,12 +1,12 @@
 struct gauss {
 	vector<ll> setOfXors;
-	void ins(ll x) {
+	void ins(ll x) {	///Add X to Gaussian Basis
 		for (auto v : setOfXors)
 			x = min(x, x ^ v);
 		if (x)
 			setOfXors.push_back(x);
 	}
-	ll qry(ll x) {
+	ll qry(ll x) { ///Get maximum subsequence XOR ^ x
 		for (auto v : setOfXors)
 			x = max(x, x ^ v);
 		return x;
